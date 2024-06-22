fed386e4568f4fbe86571795ddf5defbad612ca3
Ishan maduranga <ishanmadurangai662@gmail.com>
2024-06-22 18:50:17 +0530

    changes commited

--- main.scala
+++ main.scala
@@ -1,35 +1,15 @@
-import scala.compiletime.ops.double
-object Main {
-  def main(args: Array[String]): Unit = {
-    def area(x: Int): Double = math.Pi*x*x
-    println(area(5))
 
-    def Far(x:Int) : Double = x*1.8000 + 32.00
-    println(Far(35))
-
-    def volume(x:Int) : Double = 4/3*math.Pi*x*x*x
-    println(volume(5))
-
-    val cover_price = 24.95
-    val discount = 0.40 
-    val shippingCostFor50 = 3.00
-    val shippingCostForAdditional = 0.75
-    val copies = 60
-    val first50 = 50
-    val remain = copies - first50
-
-    def price(x: Int): Double = {
-      val discounted_price = cover_price * (1 - discount)
-      val total_price = discounted_price * x
-      val total_shipping_cost = shippingCostFor50 + shippingCostForAdditional * remain
-      total_price + total_shipping_cost
-    }
-    println(price(copies))
-
-    def easyPhase(x:Int): Int = 8*x
-    def hardPhase(x:Int): Int =  3*7
-
-    println(easyPhase(2)+ hardPhase(3) + easyPhase(2))
-
-  }
-}
+//1
+def area(r:Double) : Double = math.Pi *r*r
+//2
+def far(c:Double): Double = c*1.8 + 32
+//3
+def vol(r:Double): Double = r*r*r*math.Pi*4/3
+//4
+def disc(i:Double): Double = 24.95*0.6*i
+def ship(i:Int): Double = if(i<50) 3 else {(i-50)*0.75+3}
+def tot(i:Int):Double = disc(i) + ship(i)
+//5
+def easy(e:Double):Double = e/8.0
+def tempo(t:Double):Double = t/7.0
+def total(e:Double, t:Double) =  easy(e)+ tempo(t)
